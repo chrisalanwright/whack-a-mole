82f598cbeff1210ea2850bf784fc2eb6ec2633fc
Christopher Wright <chrisalanwright@gmail.com>
2025-09-17 17:44:02 -0400

    finished-app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useGame } from "./gameContext";
+import Field from "./field";
+import Scoreboard from "./scoreboard";
+import Welcome from "./welcome";
+
 export default function App() {
-  return <></>;
+  const { playing } = useGame();
+
+  return (
+    <>
+      <h1>Whack a mole</h1>
+      {playing ? (
+        <>
+          <Scoreboard />
+          <Field />
+        </>
+      ) : (
+        <Welcome />
+      )}
+    </>
+  );
 }
