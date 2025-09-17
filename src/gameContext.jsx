a769c9cd86410cfd60f18fe394e2ba072664e096
Christopher Wright <chrisalanwright@gmail.com>
2025-09-17 17:05:58 -0400

    more-context

--- src/gameContext.jsx
+++ src/gameContext.jsx
@@ -12,4 +12,31 @@ export function GameProvider({ children }) {
   const [playhing, setPlaying] = useState(false);
   const [time, setTime] = useState(TIME_LIMIT);
   const timer = useRef();
+
+  useEffect(() => {
+    if (time <= 0) stop();
+  }, [time]);
+
+  const whack = () => {
+    setField(makeField(field));
+    setScore(score + 1);
+  };
+
+  const start = () => {
+    setScore(0);
+    setField(makeField());
+    setPlaying(true);
+
+    time.current = setInterval(() => setTime((time) => time - 1), 1000);
+  };
+
+  const stop = () => {
+    setPlaying(false);
+
+    const newScores = [...highScores, score].sort((a, z) => z - a);
+    setHighScores(newScores.slice(0, 5));
+
+    clearInterval(timer.current);
+    setTime(TIME_LIMIT);
+  };
 }
